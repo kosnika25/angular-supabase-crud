bc695544e4ec0d6137ad933204948418bcaf9436
kosnika25 <karenvaliaodeabreu@gmail.com>
2025-09-08 13:55:19 -0300

    supabase

--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -4,8 +4,8 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 @Injectable({ providedIn: 'root' })
 export class SupabaseService {
     private supabase: SupabaseClient;
-    private supabaseUrl = 'https://angular.supabase.co';
-    private supabaseKey = 'SUA-CHAVE-ANON';
+    private supabaseUrl = 'https://awzxdbvkjrxzhvvxhacl.supabase.co';
+    private supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3enhkYnZranJ4emh2dnhoYWNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTczMTgsImV4cCI6MjA3MjMzMzMxOH0.12-1n04cJITdtglayAM1TltI3SqnP5ZgLezvc8Tyh1A';
 
     constructor() {
         this.supabase = createClient(this.supabaseUrl, this.supabaseKey);
